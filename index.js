6a0356a998e07a006b163ca0dce8b92ae0a9924f
Mohamedmadinariyaz <riyazabi.81@gmail.com>
2023-10-19 13:27:53 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// toggle menu button
+function toggleMenu(){
+    const menu = document.querySelector('.menu');
+    const nav =document.querySelector('.nav');
+    menu.classList.toggle('active');
+    nav.classList.toggle('active');
+}
+
+
